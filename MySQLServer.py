0b22fb6f43450bec9345917dc95cd8c4105195d1
bongumusa-web <bonggums@gmail.com>
2025-06-28 23:02:47 +0200

    Create MySQLServer.py

---
+++ MySQLServer.py
@@ -0,0 +1,24 @@
+
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    # Connect to MySQL server
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="qualitytech3",     # replace with your username
+        password="Bg@2024!@#"  # replace with your password
+    )
+
+    if connection.is_connected():
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+
+except Error as e:
+    print(f"Error: {e}")
+
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
